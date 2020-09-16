c29a0bf67b4d5cc085338ac28fbbd1977bce0986
kurtzilla <rob@robkurtz.net>
2020-09-16 10:53:12 -0600

    cleanup 2

--- packages/server/OBS_migrations/1599768719787-AddBasicShow.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class AddBasicShow1599768719787 implements MigrationInterface {
-    up(_: QueryRunner): Promise<void>;
-    down(__: QueryRunner): Promise<void>;
-}
--- packages/server/OBS_migrations/1599768719787-AddBasicShow.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class AddBasicShow1599768719787 implements MigrationInterface {
-  public async up(_: QueryRunner): Promise<void> {}
-
-  public async down(__: QueryRunner): Promise<void> {}
-}
